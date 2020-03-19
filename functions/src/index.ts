282811bf2a0f7d422c67eea463bb8aa9d8fefbc3
fabianhinz <hinzfabian.fh@gmail.com>
2020-03-19 18:57:07 +0100

    fetchTodaysData and get functions

---
+++ functions/src/index.ts
@@ -0,0 +1,62 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+const serviceAccount = require('../service-account.json')
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://rkicasesapi.firebaseio.com"
+})
+const firestore = admin.firestore()
+
+interface RkiData<T> {
+    state: string
+    cases: T
+    delta: string
+    rate: T
+    deaths: T
+    mostAffected: string
+}
+
+type RkiDataWithTimestamp = RkiData<number> & { timestamp: FirebaseFirestore.Timestamp }
+
+export const fetchTodaysData = functions
+    .region('europe-west1')
+    .pubsub.schedule('every 24 hours')
+    .onRun(async () => {
+        const tabletojson = (await import("tabletojson")).Tabletojson
+        const timestamp = admin.firestore.Timestamp.now()
+
+        return tabletojson.convertUrl(
+            'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html',
+            { headings: ["_", "_", "_", "state", "cases", "delta", "rate", "deaths", "mostAffected"] },
+            async tablesAsJson => {
+                for (const rkiData of (tablesAsJson[0] as RkiData<string>[])) {
+                    await firestore.collection('rkicases').doc().set({
+                        ...rkiData,
+                        timestamp,
+                        cases: Number(rkiData.cases.replace(".", "")),
+                        rate: Number(rkiData.rate.replace(",", ".")),
+                        deaths: Number(rkiData.deaths)
+                    } as RkiDataWithTimestamp)
+                }
+            }
+        )
+
+    });
+
+export const get = functions
+    .region('europe-west1')
+    .https.onRequest(async (req, res) => {
+        let query = firestore.collection("rkicases").orderBy("timestamp", "desc")
+
+        if (req.query.state) query = query.where("state", "==", req.query.state)
+        if (req.query.greaterThan) query = query.where("cases", ">", req.query.greaterThan)
+
+        return query.get().then(documentData =>
+            res.send(documentData.docs.map(doc => {
+                const { timestamp, ...rkiData } = doc.data() as RkiDataWithTimestamp
+                return { date: timestamp.toDate().toJSON(), timestamp, ...rkiData }
+            }))
+        )
+    }
+    )
